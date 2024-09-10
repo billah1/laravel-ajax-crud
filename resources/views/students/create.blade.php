c054e4bf45e949729c7130864fd33c659b0d0cf9
billah1 <billah9578@gmail.com>
2024-09-10 17:14:36 +0600

    ajax crud

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,4 +1,4 @@
-<form id="createForm" action="{{route('students.store')}}" method="POST" enctype="multipart/form-data">
+<form id="createForm" action="" method="POST" enctype="multipart/form-data">
 
     @csrf
 
